db00b2836f4bf68fef1dee9398c1bd19d3c67efa
Dienes Stein <dienes.stein@zitrus.com.br>
2025-10-21 17:52:47 -0300

    New test and fail test included.

--- cypress/e2e/test_example.cy.js
+++ cypress/e2e/test_example.cy.js
@@ -1,10 +1,18 @@
 describe('Test Cypress page', () => {
+
   it('Should visit the example page', () => {
-    cy.visit('/');
+    cy.visit('/')
     cy.title().should('include', 'Kitchen Sink')
   })
-  it.skip('Should visit the example page and fail', () => {
-    cy.visit('/');
+
+  it('Should visit the example page and fail', () => {
+    cy.visit('/')
     cy.title().should('include', 'Kitchen Sink Test with error')
   })
+
+  it('Should visit the example utilities page', () => {
+    cy.visit('/utilities')
+    cy.get('h1').should('contain.text', 'Utilities')
+  })
+
 })

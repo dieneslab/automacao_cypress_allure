36b3b88ca27107664d8d6248da71590a2098f714
Dienes Stein <dienes.stein@zitrus.com.br>
2025-10-21 16:36:27 -0300

    Environments

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,24 @@
 const { defineConfig } = require("cypress")
+const { allureCypress } = require("allure-cypress/reporter")
 const allureWriter = require("@shelex/cypress-allure-plugin/writer")
+const os = require("node:os")
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://example.cypress.io",
     setupNodeEvents(on, config) {
       allureWriter(on, config)
+      allureCypress(on, config, {
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+          runner: process.env.RUNNER_NAME || "GitHub Actions",
+          branch: process.env.GITHUB_REF_NAME || "main",
+          commit_sha: process.env.GITHUB_SHA || "unknown",
+        },
+      })
       return config;
     },
   },
